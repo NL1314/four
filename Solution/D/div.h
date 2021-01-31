0d4f567ec8f30fe4cca530f91d181be9fefa1e12
1750315928@qq.com <1750315928@qq.com>
2021-01-31 21:02:32 +0800

    添加接口

--- Solution/D/div.h
+++ Solution/D/div.h
@@ -1,4 +1,8 @@
 #include "windows.h"
+#include "add.h"
+#include "sub.h"
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
 #define EPSINON   0.000001
-
+typedef double(*LoadProc)(double x, double y);
 double div(double a, double b);
